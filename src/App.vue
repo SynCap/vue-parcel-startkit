f2802ba36101af40d2a97a00494c3ca3ffdbe01e
Constantin Losk <SynCap@users.noreply.github.com>
2019-02-21 13:12:26 +0800

    uploads

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+// src/App.vue
+
+<template lang="pug">
+.container
+	h1 {{ message }}
+</template>
+
+<style lang="stylus" scoped>
+.container
+	width: 600px;
+	margin: 50px auto;
+	text-align: center;
+</style>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Using Parcel In A Vue.js App',
+    };
+  },
+};
+</script>
